2fde75d2a0b177af14db94c200d4957add49837d
Vinicius Vieira <vinicius2335@gmail.com>
2024-09-10 14:11:09 -0300

    backend init

---
+++ back/src/main/java/com/github/vinicius2335/back/PocketApplication.java
@@ -0,0 +1,13 @@
+package com.github.vinicius2335.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocketApplication.class, args);
+	}
+
+}
